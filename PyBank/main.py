8fbf82440a170541b391f98466373e67374cab06
caeebe <47197780+caeebe@users.noreply.github.com>
2019-03-05 13:53:31 -0800

    adding results txt files and working python main files take 2

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,3 +13,69 @@ The greatest increase in profits (date and amount) over the entire period
 The greatest decrease in losses (date and amount) over the entire period
 """
 
+# First we'll import the needed modules
+import os
+import csv
+
+# the path to the dataset we are using
+budget_path = os.path.join('budget_data.csv')
+
+#initialize variables that need to start with 0
+month = 0
+months_total = 0
+net_total = 0
+num_values = 0
+great_inc = 0
+great_dec = 0
+
+with open(budget_path, newline='') as budget_file:
+    # create the object with the budget data
+    budget_reader = csv.reader(budget_file, delimiter=',')
+
+    # Read the header row first to get to the next row
+    budget_header = next(budget_reader)
+
+    # Read each row of data after the header
+    for row in budget_reader:
+
+        # count up the months
+        months_total +=1
+
+        # sum up the values of the profit/loss column
+        net_total += float(row[1])
+        # count the rows
+        num_values += 1
+        # check if this value is bigger than the previous greatest value
+        if float(row[1]) > great_inc:
+            great_inc = float(row[1])
+            great_inc_date = row[0]
+
+        # check if this value is lower than the previous lowest value
+        if float(row[1]) < great_dec:
+            great_dec = float(row[1])
+            great_dec_date = row[0]
+
+#out of FOR loops and out of the cvs reader file with all variables defined by the file
+
+total_average = net_total / num_values
+
+textfile = 'financial_analysis.txt'
+
+# Open the file in "read" mode ('r') and store the contents in the variable "text"
+with open(textfile, 'w+') as text:
+
+    text.write("Financial Analysis\n")
+    text.write("------------------\n")
+    text.write(f"Total Months: {months_total}\n")
+    text.write(f"Total Profit/loss: ${net_total}\n")
+    text.write(f"Average Profit/loss: {total_average}\n")
+    text.write(f"The greatest increase in profits happened on {great_inc_date} with ${great_inc} in profit\n")
+    text.write(f"The greatest decrease in profits happened on {great_dec_date} with ${great_dec} in loss\n")
+
+with open(textfile, 'r') as text:
+
+    # Store all of the text inside a variable called "lines"
+    lines = text.read()
+
+    # Print the contents of the text file
+    print(lines)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,3 +13,76 @@ The total number of votes each candidate won
 The winner of the election based on popular vote.
 """
 
+# First we'll import the needed modules
+import os
+import csv
+
+# the path to the dataset we are using
+election_path = os.path.join('election_data.csv')
+
+#initialize some variables that need to start with 0
+num_votes_total = 0
+most_votes = 0
+
+# we will store the information into a dictionary with each entry in the form of "name":int(# of votes)
+# and we will create a second dictionary with the percentage values "name": float(percentage)
+candidates = {}
+cand_percent = {}
+
+
+with open(election_path, newline='') as election_file:
+    # create the object with the election data
+    election_reader = csv.reader(election_file, delimiter=',')
+
+    # Read the header row first to get to the next row, we may not use this though
+    election_header = next(election_reader)
+
+    # Read each row of data after the header
+    for row in election_reader:
+        # count up the total votes cast by everyone
+        num_votes_total +=1
+        # check if this is a new candidate is in the candidate dictionary, if so add a new dictionary entry, 
+        #otherwise just update the vote value in that candidates dict entry.
+        if row[2] in candidates:
+            # add a vote to the candidate dictionary entry
+            candidates[row[2]] += 1
+        else:
+            # create a new key entry to the candidate dict and put one vote in the value
+            candidates[row[2]] = 1
+    
+# add the percentage for each candidate to the candidate dictionary, and find the candidate with the most votes
+for c in candidates:
+        percentage = 100 * candidates[c] / num_votes_total
+        cand_percent[c] = round(percentage, 3)
+        if most_votes < candidates[c]:
+            most_votes = candidates[c]
+            winner = c
+
+      
+
+#out of FOR loops and out of the cvs reader file with all variables defined by the file
+
+
+textfile = 'election_results.txt'
+
+# Open the file in "read" mode ('r') and store the contents in the variable "text"
+with open(textfile, 'w+') as text:
+    text.write("------------------\n")
+    text.write("Election Results\n")
+    text.write("------------------\n")
+    text.write(f"Total Votes: {num_votes_total}\n")
+    text.write("------------------\n")
+    #loop through all the candidates with votes
+    for c in candidates:
+        text.write(f"""{c}: {cand_percent[c]}% ({candidates[c]})\n""")
+    text.write("------------------\n")
+    text.write(f"Winner: {winner}\n")
+    text.write("------------------\n")
+
+with open(textfile, 'r') as text:
+
+    # Store all of the text inside a variable called "lines"
+    lines = text.read()
+
+    # Print the contents of the text file
+    print(lines)
